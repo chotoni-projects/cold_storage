0ab699f6099173db8d79202ae8a10011594ad3de
rifqithomirt <rifqi.arti.rt@gmail.com>
2022-11-26 15:18:18 +0700

    update table

--- resources/views/layouts/_table.blade.php
+++ resources/views/layouts/_table.blade.php
@@ -4,9 +4,20 @@
         <h4 class="text-center font-semibold text-xl">Report Temperature</h4>
         
         <div class="flex gap-4 mb-4">
-          <input id="datepicker" class="block w-max-[250px] p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 "></input>
-          <button id="submit" class="mt-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Get Data</button>
-          <button id="excel" class="mt-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Export Excel</button>
+        <div class="w-64">
+            <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select Date</label>
+            <input id="datepicker" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Range Date" required>
+        </div>
+        <div>
+            <label for="first_name" class="block mb-5 text-sm font-medium text-gray-900 dark:text-white"></label>
+            <button id="submit" class="mt-2 items-end  text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Get Data</button>
+        </div>
+        <div>
+            <label for="first_name" class="block mb-5 text-sm font-medium text-gray-900 dark:text-white"></label>
+            <button id="excel" class="mt-2 items-end  text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Export Excel</button>
+        </div>
+          <!-- <button id="submit" class="mt-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Get Data</button>
+          <button id="excel" class="mt-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Export Excel</button> -->
         </div>
       <table id="report_table" class="display" style="width:100%">
         <thead>
