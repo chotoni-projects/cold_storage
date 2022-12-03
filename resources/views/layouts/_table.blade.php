8ebb50ee0ebc6121ecb06c63e1f1b6c772a65f37
rifqithomirt <rifqi.arti.rt@gmail.com>
2022-12-04 06:50:58 +0700

    update Live Stream

--- resources/views/layouts/_table.blade.php
+++ resources/views/layouts/_table.blade.php
@@ -2,23 +2,28 @@
     <div class="bg-white rounded-lg p-4 m-2">
       <div >
         <h4 class="text-center font-semibold text-xl">Report Temperature</h4>
+        <div class="flex justify-between">
+          <div class="flex items-center mb-4 pt-10">
+              <input id="livestream" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+              <label for="livestream" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Live Update</label>
+            </div>
+          <div class="flex gap-4 mb-4">
+            
+            <div class="w-64">
+                <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select Date</label>
+                <input id="datepicker" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Range Date" required>
+            </div>
+            <div>
+                <label for="first_name" class="block mb-5 text-sm font-medium text-gray-900 dark:text-white"></label>
+                <button id="submit" class="mt-2 items-end  text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Get Data</button>
+            </div>
+            <div>
+                <label for="first_name" class="block mb-5 text-sm font-medium text-gray-900 dark:text-white"></label>
+                <button id="excel" class="mt-2 items-end  text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Export Excel</button>
+            </div>
+          </div>
+        </div>
         
-        <div class="flex gap-4 mb-4">
-        <div class="w-64">
-            <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select Date</label>
-            <input id="datepicker" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Range Date" required>
-        </div>
-        <div>
-            <label for="first_name" class="block mb-5 text-sm font-medium text-gray-900 dark:text-white"></label>
-            <button id="submit" class="mt-2 items-end  text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Get Data</button>
-        </div>
-        <div>
-            <label for="first_name" class="block mb-5 text-sm font-medium text-gray-900 dark:text-white"></label>
-            <button id="excel" class="mt-2 items-end  text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Export Excel</button>
-        </div>
-          <!-- <button id="submit" class="mt-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Get Data</button>
-          <button id="excel" class="mt-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Export Excel</button> -->
-        </div>
       <table id="report_table" class="display" style="width:100%">
         <thead>
             <tr>
@@ -37,9 +42,33 @@
 </div>
 <script>
 $(document).ready(function () {
+  var live = false;
+  var loopTable = null;
+  $('#livestream').on('change', function(){
+    live = $('#livestream').is(":checked")
+    console.log('live', live)
+    if( !live ) clearInterval(loopTable)
+    else {
+      $("#datepicker").val('')
+      loopTable = setInterval(() => {
+        console.log('live')
+        fetch('/api/logging/' ).then((response) => response.json()).then((data) => {
+            dataset = convert(data);
+            table.clear().rows.add(data).draw();
+            $('#submit').removeAttr('disabled');
+        });
+      }, 5000);
+    }
+  })
+
   $("#datepicker").flatpickr({
     mode: "range"
   });
+  $("#datepicker").on('change', function(){
+    var dateData = $('#datepicker').val()
+    $( "#livestream" ).prop( "checked", false );
+    console.log(dateData)
+  })
   dataset = [];
   var convert = function (row) {
     return row.map( function(doc){
